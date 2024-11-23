16ad9400ad8931875458401d7d808e146b3739c1
k1mb <k1mb.redkin@gmail.com>
2024-11-23 18:30:39 +0300

    init

---
+++ src/main/java/me/k1mb/edu/EduApplication.java
@@ -0,0 +1,13 @@
+package me.k1mb.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduApplication.class, args);
+    }
+
+}
